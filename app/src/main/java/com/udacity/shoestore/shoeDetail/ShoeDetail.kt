e652b6c9482801d04d212fcd3450ef672445889e
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-19 16:21:13 -0700

    added action to navigate to shoe list fragment via save and cancel buttons

--- app/src/main/java/com/udacity/shoestore/shoeDetail/ShoeDetail.kt
+++ app/src/main/java/com/udacity/shoestore/shoeDetail/ShoeDetail.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
@@ -16,14 +17,26 @@ import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
  * create an instance of this fragment.
  */
 class ShoeDetail : Fragment() {
+
 //    initialize binding
     private lateinit var binding: FragmentShoeDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+
+//        navigate to shoe list fragment when cancel button is clicked
+        binding.shoeDetailCancelButton.setOnClickListener {
+            findNavController().navigate(ShoeDetailDirections.actionShoeDetailToShoeList())
+        }
+
+//        navigate to shoe list fragment when save button is clicked
+        binding.shoeDetailSaveButton.setOnClickListener {
+            findNavController().navigate(ShoeDetailDirections.actionShoeDetailToShoeList())
+        }
 
         return binding.root
     }
