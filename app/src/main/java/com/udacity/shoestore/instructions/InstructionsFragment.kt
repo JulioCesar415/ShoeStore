c9561d1a226852ce31cd65a729def81f9340b2d0
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-19 15:52:37 -0700

    added navigate to shoeList screen via toShoeList button

--- app/src/main/java/com/udacity/shoestore/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/instructions/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
@@ -16,15 +17,20 @@ import com.udacity.shoestore.databinding.FragmentInstructionsBinding
  * create an instance of this fragment.
  */
 class InstructionsFragment : Fragment() {
+
 //    initialize binding
     private lateinit var binding: FragmentInstructionsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        inflate layout
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
-        // Inflate the layout for this fragment
+//        navigate to shoe list fragment when shoeListButton is clicked
+        binding.toShoeListButton.setOnClickListener {
+            findNavController().navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeList())
+        }
         return binding.root
     }
 
