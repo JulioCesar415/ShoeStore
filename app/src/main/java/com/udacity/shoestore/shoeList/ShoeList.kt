dc4717bc73dafd9d75b39c06b47d55230726f0f6
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-19 15:59:32 -0700

    added navigate to shoeDetail screen via addShoeDetailFAB

--- app/src/main/java/com/udacity/shoestore/shoeList/ShoeList.kt
+++ app/src/main/java/com/udacity/shoestore/shoeList/ShoeList.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
@@ -19,12 +20,18 @@ class ShoeList : Fragment() {
 
 //    initialze binding
     private lateinit var binding: FragmentShoeListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+
+//        navigate to add shoe detail fragment when add shoe detail button is clicked
+        binding.addShoeFAB.setOnClickListener {
+            findNavController().navigate(ShoeListDirections.actionShoeListToShoeDetail())
+        }
 
         return binding.root
     }
