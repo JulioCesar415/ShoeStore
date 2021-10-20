563434307b90e4f4c88a3cb45d9648aa18a2e69f
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-19 17:36:07 -0700

    mutable live data created

--- app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -3,9 +3,14 @@ package com.udacity.shoestore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
 
 
 class ShoeViewModel : ViewModel(){
+
+    private val _shoes = MutableLiveData<List<Shoe>>()
+    val shoes : LiveData<List<Shoe>>
+    get() = _shoes
 
     private val _shoeName = MutableLiveData<String>()
     val shoeName : LiveData<String>
